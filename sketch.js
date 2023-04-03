d22e2a985e5cf4bf331a35808307a3547ab4ba0b
Alexandre212121 <al2311alexandrepinheiro@gamil.com>
2023-04-03 19:55:20 -0300

    aa

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 //declarando as variáveis
-var trex, trexRunning
+var trex, trexRunning, trexCollided
 var ground, groundImage
-var cloudImage
-var c1, c2, c3, c4, c5, c6
+var cloudImage, cloudsGroup
+var c1, c2, c3, c4, c5, c6, obstaclesGroup
 var score = 0
 const play = 0
 const end = 1
 var gameState = play
+var gameOver, gameOverImg, restart, restartImg
 
 //preload carrega as mídias do jogo
 function preload() {
   //criando animação do trex correndo
   trexRunning = loadAnimation("./images/trex3.png", "./images/trex4.png")
+
   groundImage = loadImage("./images/ground2.png")
   cloudImage = loadImage("./images/cloud.png")
   c1 = loadImage("./images/obstacle1.png")
@@ -20,25 +22,43 @@ function preload() {
   c4 = loadImage("./images/obstacle4.png")
   c5 = loadImage("./images/obstacle5.png")
   c6 = loadImage("./images/obstacle6.png")
+  trexCollided = loadAnimation("./images/trex_collided.png")
+  gameOverImg = loadImage("./images/gameOver.png")
+  restartImg = loadImage("./images/restart.png")
 }
 
 
 //setup faz a configuração
 function setup() {
-  createCanvas(600, 200);
+  createCanvas(600, 200)
 
   //sprite trex
   trex = createSprite(50, 160, 20, 50)
   //adcionando animação ao trex
   trex.addAnimation("running", trexRunning)
+  trex.addAnimation("collided", trexCollided)
   trex.scale = 0.4
-  //sprite Solo
+  //trex.debug = true
+  trex.setCollider("rectangle", 0, 0, 50, 100, 40 )
+    //sprite Solo
   ground = createSprite(300, 190, 600, 20)
   ground.depth = trex.depth - 1
   ground.addImage("ground", groundImage)
   invisibleGround = createSprite(200, 195, 400, 10)
   invisibleGround.visible = false
 
+  cloudsGroup = new Group()
+  obstaclesGroup = new Group()
+
+  gameOver = createSprite(300, 80, 50, 50)
+  gameOver.visible = false
+  gameOver.addImage(gameOverImg)
+  gameOver.scale = 0.7
+
+  restart = createSprite(300, 120, 50, 50)
+  restart.addImage(restartImg)
+  restart.visible = false
+  restart.scale = 0.5
 
 
 
@@ -46,14 +66,14 @@ function setup() {
 
 //draw faz o movimento, a ação do jogo
 function draw() {
-  background(190);
+  background(190)
   text("Score: " + score, 500, 25)
-  trex.velocityY += 0.5
+  trex.velocityY += 0.7
   trex.collide(invisibleGround)
 
 
   if (gameState == play) {
-    
+
     score = Math.round(frameCount / 1.7)
 
     ground.velocityX = -8
@@ -63,29 +83,41 @@ function draw() {
     }
 
     if (keyDown("space") && trex.y >= 164) {
-      trex.velocityY = -8
-  
+      trex.velocityY = -9
+
+
+
     }
 
-    generateClouds();
-    
-    generateObs();
+    generateClouds()
+
+    generateObs()
   }
 
 
+  if (trex.isTouching(obstaclesGroup)) {
+    gameState = end
 
+  }
 
-
+  console.log(frameCount)
 
   if (gameState == end) {
-
+    ground.velocityX = 0
+    obstaclesGroup.setVelocityXEach(0)
+    cloudsGroup.setVelocityXEach(0)
+    gameOver.visible = true
+    restart.visible = true
+    trex.changeAnimation("collided")
+    obstaclesGroup.setLifetimeEach(-1)
+    cloudsGroup.setLifetimeEach(-1)
   }
   /////////////////////////////////////////////////////////////////////////// 
- 
-  
+
+
   //coordenadas do mouse na tela
   text("X: " + mouseX + " / Y: " + mouseY, mouseX, mouseY)
-  drawSprites();
+  drawSprites()
 
 }
 
@@ -100,6 +132,7 @@ function generateClouds() {
     cloud.y = random(20, 60)
     cloud.depth = trex.depth - 1
     cloud.lifetime = 700
+    cloudsGroup.add(cloud)
 
   }
 }
@@ -107,6 +140,7 @@ function generateObs() {
   if (frameCount % 75 == 0) {
     var cactus = createSprite(650, 170, 20, 20)
     cactus.velocityX = -8
+    obstaclesGroup.add(cactus)
     var rand = Math.round(random(1, 6))
     switch (rand) {
       case 1: cactus.addImage(c1)
